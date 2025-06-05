28fb9b0f9655e8721d5cd2ff684b9f9c9ee40a12
Apollo <apollo.agcaoili@gmail.com>
2025-06-06 00:15:38 +0800

    G: completed frankenstein version

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
+from stats import count_num_words
 from stats import count_each_letter
+from stats import dict_to_sorted_list
 
-def get_book_text(path_to_file):
+def process_book(path_to_file):
+
     with open(path_to_file) as f:
     # do something with f (the file) here
-    
-        # f is a file object
-        #file_contents = f.read()
-        #count_num_words(f.read())
-        count_each_letter(f.read())
+        num_words = count_num_words(f.read())
+        f.seek(0) # Reset the file pointer to the beginning
+        sorted_dict = dict_to_sorted_list(count_each_letter(f.read()))
+
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_file}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        
+        for dictionary in sorted_dict:
+            if dictionary["char"].isalpha():
+                print(f"{dictionary['char']}: {dictionary['num']}")
+                    
+        print("============= END ===============")
 
 def main():
-    get_book_text("./books/frankenstein.txt")
+    process_book("./books/frankenstein.txt")
     
 main()
--- stats.py
+++ stats.py
@@ -3,7 +3,7 @@ def count_num_words(bookstring):
     num_words = 0
     for word in wordarray:
         num_words += 1
-    print(f"{num_words} words found in the document")
+    return num_words
     
 def count_each_letter(bookstring):
     lowstring = bookstring.lower()
@@ -14,6 +14,25 @@ def count_each_letter(bookstring):
             bookdict[onechar] = 1
         else:
             bookdict[onechar] = count + 1
-    for onechar in bookdict:
-        print(f"'{onechar}':", bookdict[onechar])
+#    for onechar in bookdict:
+#        print(f"'{onechar}':", bookdict[onechar])
     return bookdict
+
+def dict_to_sorted_list(bookdict):
+    list_of_values = []
+    for key, value in bookdict.items():
+        list_of_values.append(value)
+    list_of_values.sort(reverse = True)
+    
+    list_of_dict = []
+    for target_value in list_of_values:
+
+        for key, value in bookdict.items():
+            if value == target_value:
+                temp_dict = {
+                    "char": key,
+                    "num": value
+                }
+                list_of_dict.append(temp_dict)
+                
+    return list_of_dict
