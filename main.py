aef07fbbfe76113cb804ed19a08971cd3a5d7ca1
Apollo <apollo.agcaoili@gmail.com>
2025-06-05 17:03:24 +0800

    C: count words

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("greetings boots")
 
+def count_num_words(bookstring):
+    wordarray = bookstring.split(" ")
+    num_words = 0
+    for word in wordarray:
+        num_words += 1
+    print(f"{num_words} words found in the document")
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+    # do something with f (the file) here
+    
+        # f is a file object
+        #file_contents = f.read()
+        count_num_words(f.read())
+
+def main():
+    get_book_text("./books/frankenstein.txt")
+    
+main()
