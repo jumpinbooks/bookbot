da6516924af267d9adaea2e2eeead809eda33b03
Apollo <apollo.agcaoili@gmail.com>
2025-06-05 22:21:47 +0800

    F: count letters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_num_words
+from stats import count_each_letter
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -6,7 +6,8 @@ def get_book_text(path_to_file):
     
         # f is a file object
         #file_contents = f.read()
-        count_num_words(f.read())
+        #count_num_words(f.read())
+        count_each_letter(f.read())
 
 def main():
     get_book_text("./books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -4,3 +4,16 @@ def count_num_words(bookstring):
     for word in wordarray:
         num_words += 1
     print(f"{num_words} words found in the document")
+    
+def count_each_letter(bookstring):
+    lowstring = bookstring.lower()
+    bookdict = {}
+    for onechar in lowstring:
+        count = bookdict.get(onechar, 0)
+        if count == 0:
+            bookdict[onechar] = 1
+        else:
+            bookdict[onechar] = count + 1
+    for onechar in bookdict:
+        print(f"'{onechar}':", bookdict[onechar])
+    return bookdict
